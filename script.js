02aa358a5f383c2cb72188de0967d77b970bd607
M1chaelGuerrer0 <michael.isaak.guerrero@gmail.com>
2025-11-14 22:15:09 -0800

    html file added and modified script.js

--- script.js
+++ script.js
@@ -8,32 +8,29 @@
 function binarySearch(array, key, lo, hi) {
     // base case
     if(lo > hi) {
-        return -1
+        return -1;
     }
 
     // middle calculation
-    mid = (lo + hi) / 2
+    mid = Math.floor((lo + hi) / 2);
 
     // comparisons
     if(array[mid] == key) {
-        return mid
+        return mid;
     }
     else if (array[mid] < key){
-        return binarySearch(array, key, mid+1, hi)
+        return binarySearch(array, key, mid+1, hi);
     }
     else {
-        return binarySearch(array, key, lo, mid-1)
+        return binarySearch(array, key, lo, mid-1);
     }
 }
 
 // wrapper function
-function binarySearchWrapper(array, key){
-    return binarySearch(array, key, 0, array.length - 1)
+function binarySearchWrapper(){
+    const temp = document.getElementById('array').value;
+    const array = temp.split(',').map(Number);
+    const key = document.getElementById('key').value;
+    result = binarySearch(array, key, 0, array.length - 1);
+    document.getElementById('result').textContent = result;
 }
-
-
-test_1 = [1,2,3,4,5,6,7,8,9]
-key_1 = 5
-result_1 = binarySearchWrapper(test_1, key_1)
-console.log("\nRecursive test 1: First Index\nArray: [1,2,3,4,5,6,7,8,9]\nKey: 5\nExpected Output: \n\tIndex: 4")
-console.log(result_1)
